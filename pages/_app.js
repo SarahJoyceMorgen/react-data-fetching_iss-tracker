a0d486f6e4bf853b6318a5add6b34be757368125
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-15 12:06:26 +0100

    loading, error, data

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
 import ISSTracker from "../components/ISSTracker";
+import useSWR from "swr";
 
 export default function App() {
   return <ISSTracker />;
+}
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Home() {
+  const { data, error, isLoading } = useSWR("/api/iss-data", fetcher);
+
+  return(
+<div>
+      <h1>ISS Tracker</h1>
+
+      {isLoading && <div>Loading...</div>}
+
+  
+      {error && <div>oops.. error!</div>}
+
+
+      {data && (
+        <div>
+          <p>Longitude: {data.longitude}</p>
+          <p>Latitude: {data.latitude}</p>
+        </div>
+      )}
+
+  
+    </div>
+  );
 }
